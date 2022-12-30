fa9cd8ae8b2552322255e918412d49dc840d95b6
Alexander Shpilkin <ashpilkin@gmail.com>
2022-12-30 02:39:07 +0200

    Avoid hardcoding instruction length in jumps

--- rvbase.c
+++ rvbase.c
@@ -125,7 +125,7 @@ static void bcc(struct hart *t, uint_least32_t i) {
 __attribute__((alias("jal"))) execute_t exec1B;
 
 static void jal(struct hart *t, uint_least32_t i) {
-	if (rd(i)) t->ireg[rd(i)] = t->pc + 4 & XWORD_MAX;
+	if (rd(i)) t->ireg[rd(i)] = t->nextpc;
 	t->nextpc = t->pc + jimm(i) & XWORD_MAX;
 }
 
@@ -133,7 +133,7 @@ __attribute__((alias("jalr"))) execute_t exec19;
 
 static void jalr(struct hart *t, uint_least32_t i) {
 	xword_t in = t->ireg[rs1(i)];
-	if (rd(i)) t->ireg[rd(i)] = t->pc + 4 & XWORD_MAX;
+	if (rd(i)) t->ireg[rd(i)] = t->nextpc;
 	t->nextpc = in + iimm(i) & XWORD_MAX - 1;
 }
 
